6a381f5bfa503f90c8642619c2795997694de7d4
CisnerosFranco <69135247+CisnerosFranco@users.noreply.github.com>
2020-08-08 18:06:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,44 @@
+(function(){
+    let salaPkm = document.querySelector('.sala-pokemon');
+    let options = document.getElementById('options');
+    let input = document.getElementById('selecionar');
+    let pkmSelected = document.getElementById('img-selected');
+    let btn = document.getElementById('btn');
+    let loader = document.getElementById('loader');
+    let listaPkm = [];
 
-let salaPkm = document.querySelector('.sala-pokemon');
-let options = document.getElementById('options');
-let input = document.getElementById('selecionar');
-let pkmSelected = document.getElementById('img-selected');
-let btn = document.getElementById('btn');
-let loader = document.getElementById('loader');
-let listaPkm = [];
 
 
-
-// MAIN 
-window.onload = function(){
-    loader.classList.add('mostrar')
+    // MAIN
     traerPkms(1, 100);
-    loader.classList.remove('mostrar')
-}
 
-input.addEventListener('click',()=>{
-    input.value = "";
-})
+    input.addEventListener('click',()=>{
+        input.value = "";
+    })
 
-input.addEventListener('change', ()=>{
-    loader.classList.add('mostrar')
-    
-    let selecion = input.value;
-    actualizarSelecion(selecion);
-    if(validarSelecion(selecion)){
-        btn.style.opacity = 0.7;
-    }else{
-        btn.style.opacity = 1;
+    input.addEventListener('change', ()=>{
+        loader.classList.add('mostrar')
+
+        let selecion = input.value;
+        actualizarSelecion(selecion);
+        if(validarSelecion(selecion)){
+            btn.style.opacity = 0.7;
+        }else{
+            btn.style.opacity = 1;
+        }
+        loader.classList.remove('mostrar')
+    })
+
+    btn.onclick = ()=> {
+        let pokemon = getPokemon(input.value);
+        if(pokemon != false && !validarSelecion(input.value)){
+            btn.style.opacity = 1;
+            addPkm(pokemon);
+            setDisable(input.value, 'disable');
+        }
+        else{
+            btn.style.opacity = 0.7;
+        }
     }
     
-    loader.classList.remove('mostrar')
-})
-
-btn.onclick = ()=> {
-    let pokemon = getPokemon(input.value);
-    if(pokemon != false && !validarSelecion(input.value)){
-        btn.style.opacity = 1;
-        addPkm(pokemon);
-        setDisable(input.value, 'disable');
-    }
-    else{
-        btn.style.opacity = 0.7;
-    }
-}
+})();
